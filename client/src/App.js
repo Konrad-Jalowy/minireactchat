9d49e3cf0c775aa47079dfd1bc9e1062e0ea47b1
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-11 11:19:23 +0100

    were getting somewhere

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,35 @@ import { useEffect, useState } from "react";
 const socket = io.connect("http://localhost:3001");
 
 function App() {
+
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  
+
   useEffect(() => {
     socket.emit("join_room", 1);
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
   }, [socket]);
+
+ 
+
+  const sendMessage = () => {
+    socket.emit("send_message", { message, room:1 });
+  };
   
   return (
     <div className="App">
-      
+      <input
+        placeholder="Message..."
+        onChange={(event) => {
+          setMessage(event.target.value);
+        }}
+      />
+      <button onClick={sendMessage}> Send Message</button>
+      <h1> Message:</h1>
+      {messageReceived}
     </div>
   );
 }
