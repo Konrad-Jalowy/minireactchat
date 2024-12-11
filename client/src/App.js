5bd67b09748900032c4c4daa90e046a5e4625f69
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-11 11:13:56 +0100

    needed to remove strict mode

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 
 import './App.css';
 import io from "socket.io-client";
-
+import { useEffect, useState } from "react";
 const socket = io.connect("http://localhost:3001");
 
 function App() {
-  socket.emit("join_room", 1);
+  useEffect(() => {
+    socket.emit("join_room", 1);
+  }, [socket]);
+  
   return (
     <div className="App">
       
