45feb44f0ce773a876bcedc6c0be5f886aab1630
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-11 12:00:31 +0100

    were getting somewhere

--- client/src/components/UsrNumber.js
+++ client/src/components/UsrNumber.js
@@ -1,6 +1,9 @@
+import { useEffect, useState } from "react";
+
 function UsrNumber({socket}){
+    const [usrNum, setUsrNum] = useState(1);
     return (
-        <><p>Not implemented yet</p></>
+        <><p>Number of users: {usrNum}</p></>
     );
 };
 
