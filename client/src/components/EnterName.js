4126009c279722b6800e0a9d401dc8365c424167
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-11 12:23:45 +0100

    were getting somewhere

--- client/src/components/EnterName.js
+++ client/src/components/EnterName.js
@@ -1,5 +1,21 @@
+import { useRef } from "react";
 function EnterName({socket}){
-    return (<><p>Not Implemented</p></>);
+    const ipt = useRef(null);
+
+    function onSubmitHandler(e){
+        e.preventDefault();
+        if(ipt.current.value === "")
+            return;
+        alert("submited");
+        return true;
+    }
+    return (
+    <form onSubmit={onSubmitHandler}>
+    <label htmlFor="iptName">Enter your name:</label>
+    <input ref={ipt} type="text" id="iptName" name="username"/>
+    <input type="submit" value="enter" />
+    </form>
+    );
 };
 
 export {EnterName};
--- server/server.js
+++ server/server.js
@@ -7,13 +7,16 @@ const cors = require("cors");
 app.use(cors());
 
 const server = http.createServer(app);
-let users_number = 0;
+
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
+
+let users_number = 0;
+const users = {};
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
@@ -23,6 +26,12 @@ io.on("connection", (socket) => {
 
   socket.on("send_message", (data) => {
     io.emit("receive_message", data);
+  });
+
+  socket.on('new-user', name => {
+    users[socket.id] = name;
+    io.emit('user-connected', users);
+    console.log(users);
   });
 
   socket.on('disconnect', () => {
