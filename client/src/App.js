281b4d0bc41bfe7e53cc6d0c47d24e8da013e1c5
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-11 11:07:19 +0100

    removed

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import io from "socket.io-client";
 
@@ -8,20 +8,7 @@ function App() {
   socket.emit("join_room", 1);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
