3587cdcf0c6f335acebdedc4d6cc513270b6ea63
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-11 13:22:46 +0100

    were getting somewhere

--- client/src/components/MessagesController.js
+++ client/src/components/MessagesController.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Messages } from "./Messages";
 
 function MessagesController({socket}){
+    const [messages, setMessages] = useState([]);
     return (<><p>Not implemented</p></>);
 };
 
