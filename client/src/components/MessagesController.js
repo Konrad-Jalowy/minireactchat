ff940905d6389cb9de6b152a5c17257d310ab94f
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-11 13:28:09 +0100

    beginning to work

--- client/src/components/MessagesController.js
+++ client/src/components/MessagesController.js
@@ -2,12 +2,14 @@ import { useState, useEffect } from "react";
 import { Messages } from "./Messages";
 import { EnterName } from "./EnterName";
 import { UsrNames } from "./UsrNames";
+import { UsrNumber } from "./UsrNumber";
 
 function MessagesController({socket}){
     const [messages, setMessages] = useState([]);
     const [name, setName] = useState(null);
     return (<>
     {(name === null) && <EnterName socket={socket}/>}
+    <UsrNumber socket={socket} />
     <UsrNames socket={socket} />
     <Messages messages={messages} />
     </>
