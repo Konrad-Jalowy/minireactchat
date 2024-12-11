12d01bca84c32d5ba788627595cf5504ac2e36e4
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-11 11:06:42 +0100

    cors setup correctly

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import io from "socket.io-client";
+
+const socket = io.connect("http://localhost:3001");
 
 function App() {
+  socket.emit("join_room", 1);
   return (
     <div className="App">
       <header className="App-header">
