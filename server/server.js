a799701cc4e2229fc8e4927cc53b175ca6cbca68
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-11 11:41:09 +0100

    were getting somewhere

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 app.use(cors());
 
 const server = http.createServer(app);
-
+let users_number = 0;
 const io = new Server(server, {
   cors: {
     origin: "http://localhost:3000",
@@ -17,6 +17,8 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
+  users_number++;
+  console.log(`Number of users: ${users_number}`);
 
   socket.on("send_message", (data) => {
     io.emit("receive_message", data);
