9d40603318e6a2f47193e9c9e3dea4217758ab97
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-11 13:40:55 +0100

    were getting somewhere

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,21 @@
 
 import './App.css';
 import io from "socket.io-client";
-import { useEffect, useState, useRef } from "react";
-import {Messages} from "./components/Messages";
-import { UsrNumber } from './components/UsrNumber';
-import { EnterName } from './components/EnterName';
-import { UsrNames } from './components/UsrNames';
+import {  useRef } from "react";
 import { MessagesController } from './components/MessagesController';
 
 
 function App() {
 
-  const [message, setMessage] = useState("");
-  const [messageReceived, setMessageReceived] = useState([]);
+  
   const socket = useRef(null);
   if(socket.current === null){
     socket.current = io.connect("http://localhost:3001");
   }
 
-  useEffect(() => {
-    socket.current.on("receive_message", (data) => {
-      setMessageReceived(prev => [...prev, data.message]);
-    });
-  }, [socket]);
-
-  console.log(messageReceived);
-
-  const sendMessage = () => {
-    socket.current.emit("send_message", { message, room:1 });
-  };
   
   return (
     <div className="App">
-      <input
-        placeholder="Message..."
-        onChange={(event) => {
-          setMessage(event.target.value);
-        }}
-      />
-      <button onClick={sendMessage}> Send Message</button>
-      <h1> Message:</h1>
-      { messageReceived.length > 0 && <Messages messages={messageReceived}/>}
       <MessagesController socket={socket.current}/>
     </div>
   );
