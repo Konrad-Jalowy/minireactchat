e607312ea01def5f1de9fd6b1ba0f44fe8e78b93
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-11 13:46:02 +0100

    were getting somewhere

--- client/src/components/SendMessage.js
+++ client/src/components/SendMessage.js
@@ -1,14 +1,19 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 function SendMessage({socket}){
     const [message, setMessage] = useState("");
+    const iptRef = useRef(null);
 
     const sendMessage = () => {
+        if(iptRef.current.value === "")
+            return;
+        iptRef.current.value = "";
         socket.emit("send_message", {message});
       };
 
     return (
     <><input
+    ref={iptRef}
     placeholder="Message..."
     onChange={(event) => {
       setMessage(event.target.value);
