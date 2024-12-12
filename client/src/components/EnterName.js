375eb93c203b2f937db3153ef6158f097b2b955c
Konrad-Jalowy <sitesystem@wp.pl>
2024-12-12 12:24:01 +0100

    changes

--- client/src/components/EnterName.js
+++ client/src/components/EnterName.js
@@ -11,8 +11,11 @@ function EnterName({socket, setName}){
     }
     return (
     <form onSubmit={onSubmitHandler}>
+    <div className="form-row">
     <label htmlFor="iptName">Enter your name:</label>
     <input ref={ipt} type="text" id="iptName" name="username"/>
+    </div>
+    
     <input type="submit" value="enter" />
     </form>
     );
